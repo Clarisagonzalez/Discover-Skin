2c1aa22cc33bb9f60c0c9f234a1afe1ef7f380e3
00shivani <shivanisingh13@icloud.com>
2023-10-15 21:20:05 -0400

    edited skincare_research.json and quiz.js to make the quiz responses correlate with the keys in the database

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,77 +1,61 @@
-// ISSUES
-// 1. headline is not changing per question
-// 2. how to add multiple answers? (checkboxes?)
-// possible solution: add a next button so screen doesnt change out -- done
-// disable a button after its been clicked once... unclick it
-
-// answer buttons: 1) clicking it once stores data, clicking twice unstores data. 2) for all questions except the serum question, upon clicking one answer, all other buttons should disable unless that button is clicked again, then they will be re-enabled. 3) for only the serum question, the buttons will not disable after clicking a button. clicking one button twice will still unstore the data. 
-// local storage used for to api call
-// change the color to match disable and enable state
-
-// clicking it again should unstore data
-// 3. change font to montserrat + colors to match theme
-// 4. remove correct/incorrect and store info instead
-// 5. add popover for SPF answers so they don't look so wordy
-// 6. progress stepper which highlights the skin routine as well as quiz progress on the left. 
-
-// Lots of commented out stuff at the bottom. 
 
 var questions = [
   {
     headline: "Let's begin by creating your base routine, comprising of the ideal cleanser, makeup remover, and moisturizer type.",
     question: "What is your skin type?",
       answers: [
-          {text: "Oily and/or acne prone", correct: false},
-          {text: "Dry, red, and/or exzema prone", correct: true},
-          {text: "Sensitive", correct: false},
-          {text: "Mature", correct: false},
-          {text: "Normal", correct: false},
+          {text: "Normal and/or any skin type"},
+          {text: "Oily and/or acne prone"},
+          {text: "Dry, red and/or eczema prone"},
+          {text: "Combination"},
+          {text: "Inflamed and/or sensitive"},
+          {text: "Mature"},
       ]
   },
   {
       headline: "Let's move on to the eyes.",
       question: "What are your eye area concerns?",
       answers: [
-          {text: "Undereye bags and/or inflammation", correct: true},
-          {text: "Undereye discoloration and/or visible veins", correct: false},
+          {text: "Under eye bags and/or inflammation"},
+          {text: "Under eye discoloration and/or visible veins"},
       ]
   },
   {
-      headline: "Serums give your skin the targetted boost its looking for. You can select as many skincare goals as you want, but we reccommend a maximum usage of three serums per day, otherwise your skin may be overwhelmed",
-      question: "For your custom serum routine, select your primary skincare goals:",
+      headline: "Serums give your skin the targetted boost its looking for.",
+      question: "To add custom-curated serum ingredients to your routine, select your primary skincare goal:",
       answers: [
-          {text: "Hydration", correct: false},
-          {text: "Brightening", correct: true},
-          {text: "Anti-aging", correct: false},
-          {text: "Relieving skin sensitivity", correct: false},
+          {text: "Hydration"},
+          {text: "Brightening"},
+          {text: "Anti-aging"},
+          {text: "Relieving skin sensitivity"},
       ]
   },
   {
       headline: "Toners give your pores a deep clean, and when used regularly, it can have a major positive impact on the appearance and tightness of your pores.",
-      question: "Select one primary skincare goal to identify the perfect toner ingredients for you:",
+      question: "Select your primary skincare goal to identify the perfect toner ingredients for you:",
       answers: [
-          {text: "Maintaining clean pores", correct: false},
-          {text: "Hydration and/or fine line prevention", correct: true},
-          {text: "Relieving skin sensitivity", correct: false},
-          {text: "Anti-aging", correct: false},
+          {text: "Maintaining clean pores"},
+          {text: "Hydration and/or fine line prevention"},
+          {text: "Relieving skin sensitivity"},
+          {text: "Anti-aging"},
       ]
   },
   {
       headline: "We all know the importance of SPF, as UV rays are one of the primary causes for aging.",
       question: "Study the descriptions of each ingredient group and select the type of sunblocker that works best for your skin.",
       answers: [
-          {text: "Chemical - Chemical ingredients, such as oxybenzone and octinoxate, are absorbed into your skin to counteract the damage from UV rays.", },
-          {text: "Physical - Physical (or natural) ingredients, such as titanium dioxide and zinc oxide, sit on top of the skin to physcially deflext and prevent UV rays from entering the body. ", },
-          {text: "Combination - A combination of chemical and physical ingredients provide protection from both UVA and UVB rays, and typically do not create any visible residue. This is the perfect middle ground for those with sensitive skin and don't want a white/grayish tint left behind on their skin. ", },
+          {text: "Chemical - Chemical ingredients, such as oxybenzone and octinoxate, are absorbed into your skin to counteract the damage from UV rays. PROS: They are beneficial because they are lightweight, easy to apply, and transparent on the skin. CONS: However, the chemicals can irritate and cause reactions in those with sensitive skin.Certain ingredients like oxybenzone have raised health concerns and carry a high hazard\" rating on the EWG's Skin Deep Cosmetic Database.\""},
+          {text: "Physical - Physical, or natural, ingredients, such as titanium dioxide and zinc oxide, sit on top of the skin to physically deflect and prevent UV rays from entering the body. PROS: It is beneficial because there is very little risk of irritation or health concerns. CONS: However, it can often leave a white or grayish tint on the skin, particularly in those with darker skin tones."},
+          {text: "Hybrid - A hybrid of chemical and physical ingredients provide protection from both UVA and UVB rays, and typically do not create any visible residue. This is the perfect middle ground for those with sensitive skin and don't want a white/grayish tint left behind on their skin."},
       ]
   },
   {
       headline: "Last but not least, lips need loving too!",
       question: "Select your primary lip care concern.",
       answers: [
-          {text: "dehydration and/or frequently chapped lips", correct: false},
-          {text: "hyperpigmentation of the lips", correct: false},
-          {text: "dullness", correct: true},
+          {text: "Dehydration and/or frequently chapped lips"},
+          {text: "Hyperpigmentation of the lips"},
+          {text: "Dullness"},
       ]
   },
 ];
